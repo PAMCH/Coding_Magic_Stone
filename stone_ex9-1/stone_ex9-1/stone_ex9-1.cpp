2173928acdddf547d958380c366b88ad051dd199
PAMCH <pamic7124@naver.com>
2023-02-04 22:44:32 +0900

    Solved stone ex9-1

---
+++ stone_ex9-1/stone_ex9-1/stone_ex9-1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("%d+%d+%d=%d\n", a, b, c, a + b + c);
+    printf("%d-%d-%d=%d\n", a, b, c, a - b - c);
+    printf("%d*%d*%d=%d\n", a, b, c, a * b * c);
+    printf("%d/%d/%d=%d\n", a, b, c, a / b / c);
+
+    return 0;
+}
