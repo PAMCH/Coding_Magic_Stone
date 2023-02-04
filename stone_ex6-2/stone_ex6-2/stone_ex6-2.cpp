990e0cae6e4defc767bc42ee4c9480d6b7ea6e81
PAMCH <pamic7124@naver.com>
2023-02-04 21:36:39 +0900

    Solved stone ex6-2

---
+++ stone_ex6-2/stone_ex6-2/stone_ex6-2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    float a = 12.34, b = 23.12;
+
+    printf("%.2lf + %.2lf = %.2lf\n", a, b, a + b);
+    printf("%.2lf - %.2lf = %.2lf\n", a, b, a - b);
+    printf("%.2lf * %.2lf = %.2lf\n", a, b, a * b);
+    printf("%.2lf / %.2lf = %.2lf\n", a, b, a / b);
+
+    return 0;
+}
