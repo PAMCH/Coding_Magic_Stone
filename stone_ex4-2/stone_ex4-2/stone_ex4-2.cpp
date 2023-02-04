501e1f9d2ecebcfa4d06426bcb55cf6a42a91131
PAMCH <pamic7124@naver.com>
2023-02-04 20:09:39 +0900

    Solved stone_ex4-2

---
+++ stone_ex4-2/stone_ex4-2/stone_ex4-2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    printf("%10d\n", 1);
+    printf("%10d\n", 11);
+    printf("%10d\n", 111);
+    printf("%10d\n", 1111);
+    printf("%10d\n", 11111);
+    printf("----------\n");
+    printf("%10d\n", 1 + 11 + 111 + 1111 + 11111);
+
+    return 0;
+}
