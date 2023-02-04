1e5c1c3d4f1f9d07e304718f949bd0f8d943884c
PAMCH <pamic7124@naver.com>
2023-02-04 22:12:28 +0900

    Solved stone ex7-2

---
+++ stone_ex7-2/stone_ex7-2/stone_ex7-2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	double a, b;
+
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+
+	printf("%.2lf+%.2lf=%.2lf\n", a, b, a + b);
+	printf("%.2lf-%.2lf=%.2lf\n", a, b, a - b);
+	printf("%.2lf*%.2lf=%.2lf\n", a, b, a * b);
+	printf("%.2lf/%.2lf=%.2lf\n", a, b, a / b);
+
+	return 0;
+}
