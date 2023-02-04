f5842b5875e6dc2b2347f0e6b824cbf39c13df03
PAMCH <pamic7124@naver.com>
2023-02-04 20:58:46 +0900

    Solved stone ex5-2

---
+++ stone_ex5-2/stone_ex5-2/stone_ex5-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	printf("%10.3lf\n", 12.567);
+	printf("%10.3lf\n", 456.777);
+	printf("%10.3lf\n", 123456.780);
+	printf("%10.3lf\n", 4567.568);
+	printf("%10.3lf\n", 6712.345);
+	
+	return 0;
+}
