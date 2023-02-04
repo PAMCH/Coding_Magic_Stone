28514d2828c5e4d6ac4b0dca9feccc2977f0a255
PAMCH <pamic7124@naver.com>
2023-02-04 22:48:21 +0900

    Solved stone ex9-2

---
+++ stone_ex9-2/stone_ex9-2/stone_ex9-2.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    char a, b;
+
+    scanf("%c %c", &a, &b);
+
+    printf("%c:%d\n", a, a);
+    printf("%c:%d\n", b, b);
+
+    return 0;
+}
