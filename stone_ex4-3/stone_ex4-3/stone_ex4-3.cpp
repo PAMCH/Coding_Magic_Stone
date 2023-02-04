64c20819c3773cb4c5c05e81ae5d74d90d0627ba
PAMCH <pamic7124@naver.com>
2023-02-04 20:14:36 +0900

    Solved stone_ex4-3

---
+++ stone_ex4-3/stone_ex4-3/stone_ex4-3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	printf("%5d\n", 22222);
+	printf("%5d\n", 2222);
+	printf("%5d\n", 222);
+	printf("%5d\n", 22);
+	printf("%5d\n", 2);
+	printf("-----\n");
+	printf("%5d\n", 22222 - 2222 - 222 - 22 - 2);
+
+	return 0;
+}
