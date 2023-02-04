57ad8df0d0d8c4265c9c619595156d16ddc6aeda
PAMCH <pamic7124@naver.com>
2023-02-04 21:40:13 +0900

    Change Directory Name stone ex3-2

---
+++ stone_ex3-2/stone_ex_3_2/stone_ex_3_2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "    #\n";
+    std::cout << "   ###\n";
+    std::cout << "  #####\n";
+    std::cout << " #######\n";
+    std::cout << "#########\n";
+    std::cout << " #######\n";
+    std::cout << "  #####\n";
+    std::cout << "   ###\n";
+    std::cout << "    #\n";
+}
