eafabad590628b33b9c4882a1f5cdd7d707d9489
PAMCH <pamic7124@naver.com>
2023-02-04 19:48:03 +0900

    Solved stone ex3-3

---
+++ stone_ex3-3/stone_ex3-3/stone_ex3-3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << "EEEEEEE U     U L       EEEEEEE RRRRRR\n";
+    std::cout << "E       U     U L       E       R     R\n";
+    std::cout << "EEEEEEE U     U L       EEEEEEE RRRRRRR\n";
+    std::cout << "E       U     U L       E       R    R\n";
+    std::cout << "EEEEEEE UUUUUUU LLLLLLL EEEEEEE R     R\n";
+}
