c2cd3a4eb260b5de68297a1f217625bb5039b27b
PAMCH <pamic7124@naver.com>
2023-02-04 21:31:31 +0900

    Solved stone ex6-1

---
+++ stone_ex6-1/stone_ex6-1/stone_ex6-1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 54, b = 32;
+
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+
+    return 0;
+}
