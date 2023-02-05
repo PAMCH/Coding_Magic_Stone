77a5077a42326ea9a538c773f22ea99d6c41ef5d
PAMCH <pamic7124@naver.com>
2023-02-05 22:27:19 +0900

    Solved

---
+++ stone_OOJ_1007/stone_OOJ_1007/stone_OOJ_1007.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, sum = 0;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        sum += i * (a - i + 1);
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
