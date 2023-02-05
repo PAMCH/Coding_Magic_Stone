ca96fcc0514890260377918dc6786c2d1484465a
PAMCH <pamic7124@naver.com>
2023-02-05 21:31:32 +0900

    Solved stone OOJ 2004

---
+++ stone_OOJ_2004/stone_OOJ_2004/stone_OOJ_2004.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    if (a <= b) {
+        printf("2\n");
+    }
+    else if ((a * 2) % b == 0) {
+        printf("%d\n", (a * 2) / b);
+    }
+    else {
+        printf("%d\n", (a * 2) / b + 1);
+    }
+
+    return 0;
+}
