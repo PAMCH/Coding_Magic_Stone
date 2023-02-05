131c4af91d8ffa7af4d415b04d52d10633b52ecd
PAMCH <pamic7124@naver.com>
2023-02-05 14:26:02 +0900

    Solved stone OOJ 1110

---
+++ stone_OOJ_1110/stone_OOJ_1110/stone_OOJ_1110.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d", &a);
+
+    b = a / 2;
+    c = a - b;
+
+    printf("%d\n", (b + 1) * (c + 1));
+
+    return 0;
+}
