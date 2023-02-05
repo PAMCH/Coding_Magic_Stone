77f15a1a5d57c2231f7c6536d5638b91f4f575a3
PAMCH <pamic7124@naver.com>
2023-02-05 15:03:19 +0900

    Solved stone ex13-2

---
+++ stone_ex13-2/stone_ex13-2/stone_ex13-2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	if ((a + b) > 0) {
+		printf("Natural Number\n");
+	}
+	else {
+		printf("0 or Negative Number\n");
+	}
+
+	return 0;
+}
