0828a313d00e1a3e71aea3069f60c64f28730f6b
PAMCH <pamic7124@naver.com>
2023-02-05 14:59:23 +0900

    Solved stone ex13-1

---
+++ stone_ex13-1/stone_ex13-1/stone_ex13-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    if ((a + b) % 2 == 0) {
+        printf("even\n");
+    }
+    else {
+        printf("odd\n");
+    }
+
+
+    return 0;
+}
