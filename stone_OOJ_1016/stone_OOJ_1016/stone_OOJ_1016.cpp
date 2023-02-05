9c9ce12bd171e734941aa476eed71897ecc0ac26
PAMCH <pamic7124@naver.com>
2023-02-05 20:37:47 +0900

    Solved stone OOJ 1016

---
+++ stone_OOJ_1016/stone_OOJ_1016/stone_OOJ_1016.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    if (a % 2 == 0 || b % 2 == 1) {
+        printf("1\n");
+    }
+    else {
+        printf("0\n");
+    }
+
+    return 0;
+}
