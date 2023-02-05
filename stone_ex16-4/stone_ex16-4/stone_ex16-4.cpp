b2a488f0332a52e67b313cb55e09716718fbbeb3
PAMCH <pamic7124@naver.com>
2023-02-05 21:54:24 +0900

    Solved stone ex16-4

---
+++ stone_ex16-4/stone_ex16-4/stone_ex16-4.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	for (int i = a; i >= b; i--) {
+		printf("%d ", i);
+	}
+
+	return 0;
+}
