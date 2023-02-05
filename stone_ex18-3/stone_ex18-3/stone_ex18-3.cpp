dbca9857cd2b9005f69869df2c111f4de0583845
PAMCH <pamic7124@naver.com>
2023-02-05 23:14:35 +0900

    Solved

---
+++ stone_ex18-3/stone_ex18-3/stone_ex18-3.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, cnt = 0;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0) {
+            cnt++;
+        }
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
