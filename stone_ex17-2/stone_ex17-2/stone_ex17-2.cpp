a639a6170b9496a9e805450a1ae90a97300d421f
PAMCH <pamic7124@naver.com>
2023-02-05 22:35:53 +0900

    Solved

---
+++ stone_ex17-2/stone_ex17-2/stone_ex17-2.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+
+    printf("%d", (a + b) * (b - a + 1) / 2);
+
+    return 0;
+}
