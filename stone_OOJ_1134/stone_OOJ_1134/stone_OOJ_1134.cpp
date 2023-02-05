13cc1ce84ffc697599e4802cf7ec8a58f2a52e41
PAMCH <pamic7124@naver.com>
2023-02-06 00:12:05 +0900

    Solved

---
+++ stone_OOJ_1134/stone_OOJ_1134/stone_OOJ_1134.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+
+    /*
+    int cnt = 0;
+
+    for (int i = 1; i < a; i++) {
+        if (i % 4 == 0) {
+            cnt++;
+        }
+    }
+    */
+
+    /*
+    for (int i = 4; i < a; i += 4) {
+        cnt++;
+    }
+    */
+
+    printf("%d", (a-1)/4);
+
+    return 0;
+}
