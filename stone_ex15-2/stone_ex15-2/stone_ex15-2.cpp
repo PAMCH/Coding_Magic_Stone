8f99387a7c1b9011ca067267e7aef6194ee5c7d0
PAMCH <pamic7124@naver.com>
2023-02-05 21:01:30 +0900

    Solved stone ex15-2

---
+++ stone_ex15-2/stone_ex15-2/stone_ex15-2.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	if (a == 1 && b == 1) {
+		printf("1\n");
+	}
+	else if (a == 1 || b == 1) {
+		printf("2\n");
+	}
+	else {
+		printf("3\n");
+	}
+}
