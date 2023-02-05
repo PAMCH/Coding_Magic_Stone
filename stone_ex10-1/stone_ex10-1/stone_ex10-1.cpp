226f7743f56125cfe2144751f76b83b950b4aa3e
PAMCH <pamic7124@naver.com>
2023-02-05 13:11:25 +0900

    Solved stone ex10-1

---
+++ stone_ex10-1/stone_ex10-1/stone_ex10-1.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    /*
+    int a, b, c, d, e;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+    scanf("%d", &e);
+
+    printf("%d\n", a);
+    printf("%d\n", a + b);
+    printf("%d\n", a + b + c);
+    printf("%d\n", a + b + c + d);
+    printf("%d\n", a + b + c + d + e);
+    */
+
+    int a, sum = 0;
+
+    scanf("%d", &a);
+    sum += a;
+    printf("%d\n", sum);
+
+    scanf("%d", &a);
+    sum += a;
+    printf("%d\n", sum);
+
+    scanf("%d", &a);
+    sum += a;
+    printf("%d\n", sum);
+
+    scanf("%d", &a);
+    sum += a;
+    printf("%d\n", sum);
+
+    scanf("%d", &a);
+    sum += a;
+    printf("%d\n", sum);
+
+    return 0;
+}
