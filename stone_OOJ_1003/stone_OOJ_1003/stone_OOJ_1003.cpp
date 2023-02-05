d33c5cf5be84fe95a6c35463bf0e0c2a5f05e19f
PAMCH <pamic7124@naver.com>
2023-02-05 23:19:28 +0900

    Solved

---
+++ stone_OOJ_1003/stone_OOJ_1003/stone_OOJ_1003.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, even = 0, odd = 0;
+
+	scanf("%d", &a);
+
+	for (int i = 1; i <= a; i++) {
+		if (i % 2 == 0) {
+			even += i;
+		}
+		else {
+			odd += i;
+		}
+	}
+
+	printf("%d\n", even);
+	printf("%d\n", odd);
+
+	return 0;
+}
