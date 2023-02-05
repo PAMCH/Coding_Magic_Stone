4dc3a4aa421c454dc82a8735bbec847ad154f422
PAMCH <pamic7124@naver.com>
2023-02-05 14:44:58 +0900

    Solved stone ex12-2

---
+++ stone_ex12-2/stone_ex12-2/stone_ex12-2.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if ((a + b) % 2 == 0) {
+        printf("even\n");
+    }
+    if ((a + b) % 2 != 0) {
+        printf("odd\n");
+    }
+
+    return 0;
+}
