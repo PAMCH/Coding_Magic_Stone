abbee539a16c2ce7577dee38a511e094e73fec8a
PAMCH <pamic7124@naver.com>
2023-02-05 13:56:58 +0900

    Solved stone OOJ 1131

---
+++ stone_OOJ_1131/stone_OOJ_1131/stone_OOJ_1131.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d", &a, &b);
+    scanf("%d", &c);
+
+    printf("%d %d\n", (a + (b + c) / 60) % 24, (b + c) % 60);
+
+    return 0;
+}
