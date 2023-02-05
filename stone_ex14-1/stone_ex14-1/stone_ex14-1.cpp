3ba42bb07c6bdf2165e3e7fa2481236318880e78
PAMCH <pamic7124@naver.com>
2023-02-05 20:14:45 +0900

    Solved stone ex14-1

---
+++ stone_ex14-1/stone_ex14-1/stone_ex14-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+
+    if (a >= 1 && a <= 10) {
+        printf("1 or more and 10 or less\n");
+    }
+    else {
+        printf("less then 1 or greater then 10\n");
+    }
+
+    return 0;
+}
