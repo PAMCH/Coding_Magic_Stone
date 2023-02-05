aebdfce62966c72978213e07bd7e7f8a4944cfb5
PAMCH <pamic7124@naver.com>
2023-02-05 14:49:46 +0900

    Solved stone OOJ 1001

---
+++ stone_OOJ_1001/stone_OOJ_1001/stone_OOJ_1001.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf(">\n");
+    }
+    if (a < b) {
+        printf("<\n");
+    }
+    if (a == b) {
+        printf("=\n");
+    }
+    return 0;
+}
