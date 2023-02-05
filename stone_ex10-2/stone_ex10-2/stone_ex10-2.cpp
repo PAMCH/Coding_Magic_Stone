b739593457e99533dba9d73e57f6f8c15b3912ad
PAMCH <pamic7124@naver.com>
2023-02-05 13:16:54 +0900

    Solved stone ex10-2

---
+++ stone_ex10-2/stone_ex10-2/stone_ex10-2.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	
+	scanf("%d %d %d", &a, &b, &c);
+
+	printf("%d\n", a + b + c);
+	//printf("%.2lf\n", (double(a) + double(b) + double(c)) / 3);
+	printf("%.2lf\n", double(a + b + c)/ 3);
+
+	return 0;
+}
