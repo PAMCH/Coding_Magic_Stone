394385529100666af6d8bff7460fbe86ab0b8f75
PAMCH <pamic7124@naver.com>
2023-02-05 22:06:37 +0900

    Solved stone ex16-6

---
+++ stone_ex16-6/stone_ex16-6/stone_ex16-6.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b, sum = 0;
+
+	scanf("%d %d", &a, &b);
+
+	for (int i = a; i <= b; i++) {
+		sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
