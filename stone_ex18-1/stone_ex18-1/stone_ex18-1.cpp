01e58f13819b3c13703ef387071d2c9a91a75375
PAMCH <pamic7124@naver.com>
2023-02-05 23:04:29 +0900

    Solved

---
+++ stone_ex18-1/stone_ex18-1/stone_ex18-1.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i += 2) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
