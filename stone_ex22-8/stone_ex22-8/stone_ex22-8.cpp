a595e7903ec931f21187b17f5670f89e7b36ab6a
PAMCH <pamic7124@naver.com>
2023-02-06 23:15:55 +0900

    Solved

---
+++ stone_ex22-8/stone_ex22-8/stone_ex22-8.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 1; j <= i - 1; j++) {
+			printf(" ");
+		}
+
+		for (int j = i*2-1; j <= 9; j++) {
+			printf("%d", j);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
