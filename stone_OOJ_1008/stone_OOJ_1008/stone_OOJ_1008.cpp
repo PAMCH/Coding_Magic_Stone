f65752d9ce6642ed266e2d0fc8ddaf46c310fe85
PAMCH <pamic7124@naver.com>
2023-02-06 21:41:30 +0900

    Solved

---
+++ stone_OOJ_1008/stone_OOJ_1008/stone_OOJ_1008.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, fac = 1;
+	scanf("%d", &a);
+
+	printf("%d!=(%d", a, fac);
+
+	for (int i = 2; i <= a; i++) {
+		fac *= i;
+		printf("*%d", i);
+	}
+
+	printf(")=%d", fac);
+
+	return 0;
+}
