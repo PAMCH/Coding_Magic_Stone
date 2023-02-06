6126a66b97f7a291d0bbdeee95915f2d94146941
PAMCH <pamic7124@naver.com>
2023-02-06 23:41:30 +0900

    Solved

---
+++ stone_OOJ_2000/stone_OOJ_2000/stone_OOJ_2000.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, cnt = 0;
+
+	scanf("%d", &a);
+
+	for (int i = 1; i <= 10; i++) {
+		for (int j = i+1; j <= 10; j++) {
+			for (int k = j+1; k <= 10; k++) {
+				if (i + j + k == a) {
+					printf("%d %d %d\n", i, j, k);
+					cnt += 1;
+				}
+			}
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
