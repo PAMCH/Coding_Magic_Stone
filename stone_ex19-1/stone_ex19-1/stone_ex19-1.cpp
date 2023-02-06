34410c7c9e03ea947dc10b45facd59016a422ab3
PAMCH <pamic7124@naver.com>
2023-02-06 21:26:01 +0900

    Solved

---
+++ stone_ex19-1/stone_ex19-1/stone_ex19-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, sum = 0;
+	scanf("%d", &a);
+
+	for (int i = 1; i <= a - 1; i++) {
+		if(a % i == 0)
+			sum += i;
+	}
+
+	if (sum == a) {
+		printf("PERFECT\n");
+	}
+	else if (sum > a) {
+		printf("ABUNDANT\n");
+	}
+	else {
+		printf("DEFICIENT\n");
+	}
+}
