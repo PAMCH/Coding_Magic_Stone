d3e27f876e709e6238fe6600ddf6ece0b179445b
PAMCH <pamic7124@naver.com>
2023-02-06 21:50:06 +0900

    Solved

---
+++ stone_ex21-1/stone_ex21-1/stone_ex21-1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	//int i, j;
+
+	for (int i = 1; i <= 5; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf("#");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
