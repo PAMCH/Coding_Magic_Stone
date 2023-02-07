c6c1568f437dc0b72508cdfbd89c83139fa01cfd
PAMCH <pamic7124@naver.com>
2023-02-07 23:48:42 +0900

    Solved

---
+++ stone_OOJ_1048/stone_OOJ_1048/stone_OOJ_1048.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a1, a2, r = 0;
+
+    scanf("%d", &a1);
+
+    a2 = a1;
+
+    while (a1 != 0) {
+        r = r * 10 + a1 % 10;
+        a1 /= 10;
+    }
+
+    printf("%d\n", a2 + r);
+
+    return 0;
+}
