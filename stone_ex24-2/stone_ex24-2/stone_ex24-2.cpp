36f6f6e1946a212718e6ec70409d53e2cfd4f289
PAMCH <pamic7124@naver.com>
2023-02-07 22:48:37 +0900

    Solved

---
+++ stone_ex24-2/stone_ex24-2/stone_ex24-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a = 0, cnt = 0;
+
+    do {
+        a += 3;
+        printf("%d ", a);
+        cnt++;
+        if (cnt % 10 == 0) {
+            printf("\n");
+        }
+    } while ( a < 99 );
+
+    return 0;
+}
