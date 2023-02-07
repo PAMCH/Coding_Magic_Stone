b34be531aebdd6113bba47e3cbbb89b0b95f333f
PAMCH <pamic7124@naver.com>
2023-02-07 23:17:35 +0900

    Solved

---
+++ stone_ex25-1/stone_ex25-1/stone_ex25-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, n, r = 0;
+    scanf("%d", &a);
+
+    n = a;
+
+    while (a != 0) {
+        r = r * 10 + a % 10;
+        a /= 10;
+    }
+    if (n == r) {
+        printf("Palindrome Number\n");
+    }
+    else {
+        printf("Normal Number");
+    }
+
+    return 0;
+}
