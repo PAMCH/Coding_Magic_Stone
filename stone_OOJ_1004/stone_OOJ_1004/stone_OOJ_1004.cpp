efb8580b0af743e8d250275bf05c4b173ca8a2b2
PAMCH <pamic7124@naver.com>
2023-02-07 21:48:31 +0900

    Solved

---
+++ stone_OOJ_1004/stone_OOJ_1004/stone_OOJ_1004.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, sum = 0;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        if (i % 2 == 1) {
+            sum += (i * i);
+        }
+        else {
+            sum -= (i * i);
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
