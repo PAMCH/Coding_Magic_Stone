c354d0782520e81ae28942acb0e4f4daf4091b5c
PAMCH <pamic7124@naver.com>
2023-02-07 22:54:06 +0900

    Solved

---
+++ stone_OOJ_1018/stone_OOJ_1018/stone_OOJ_1018.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d;
+
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+
+    while (a < c) {
+        a += b;
+        c -= d;
+    }
+
+    printf("%d", a);
+
+    return 0;
+}
