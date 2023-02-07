9ee73ad41a16775333c64ea1d00180d2de100aee
PAMCH <pamic7124@naver.com>
2023-02-07 23:04:14 +0900

    Solved

---
+++ stone_OOJ_2085/stone_OOJ_2085/stone_OOJ_2085.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, sum = 0, day = 0, gold = 0;
+
+    scanf("%d", &a);
+
+    while (a > day) {
+        gold++;
+        day += gold;
+        sum += (gold * gold);
+    }
+
+    sum -= (day - a) * gold;
+
+    printf("%d\n", sum);
+
+    return 0;
+}
