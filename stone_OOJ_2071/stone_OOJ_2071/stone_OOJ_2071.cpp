5480c0a9cfd9a0ef8c9baa3b3d4079fb687c98a4
PAMCH <pamic7124@naver.com>
2023-02-07 22:40:07 +0900

    Solved

---
+++ stone_OOJ_2071/stone_OOJ_2071/stone_OOJ_2071.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a, i, n1, n2, cnt = 0;
+
+    scanf("%d", &a);
+
+    for (int i = 1; i <= 500; i++) {
+        n2 = i * i + a;
+        n1 = sqrt(n2);
+        if (n1 * n1 == n2) {
+            cnt++;
+        }
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
