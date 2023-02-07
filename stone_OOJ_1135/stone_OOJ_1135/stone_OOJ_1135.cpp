3024d44649b255823b3192c099a728035ca014e1
PAMCH <pamic7124@naver.com>
2023-02-07 21:59:51 +0900

    Solved

---
+++ stone_OOJ_1135/stone_OOJ_1135/stone_OOJ_1135.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b = 0, sum = 0;
+
+    scanf("%d", &a);
+
+    /*
+    for (int i = 1; i <= a; i++) {
+
+        b = 0;
+
+        for (int j = 1; j <= i * 2 - 1; j += 2) {
+            b += j;
+        }
+
+        sum += b;
+    }
+    */
+
+    for (int i = 1; i <= a; i++) {
+        /*
+        b += (i * 2 - 1);
+        sum += b;
+        */
+        sum += i * i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
