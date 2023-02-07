89ae27dd02b2435926fe2c522d8b54132c8a6848
PAMCH <pamic7124@naver.com>
2023-02-07 22:32:42 +0900

    Solved

---
+++ stone_OOJ_1143/stone_OOJ_1143/stone_OOJ_1143.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a, i;
+
+    scanf("%d", &a);
+
+    // for (i = 1; i * i < a; i++);
+    //printf("%d", i);
+
+    printf("%d\n", int(sqrt(a)));
+
+    return 0;
+}
