b1cde7356dddcc3e79bfb363dd8192ba9a9f2304
PAMCH <pamic7124@naver.com>
2023-02-07 23:26:43 +0900

    Solved

---
+++ stone_OOJ_1043/stone_OOJ_1043/stone_OOJ_1043.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a1, a2, r1 = 0, r2 = 0;
+    scanf("%d %d", &a1, &a2);
+
+    while (a1 != 0) {
+        r1 = r1 * 10 + a1 % 10;
+        a1 /= 10;
+    }
+    while (a2 != 0) {
+        r2 = r2 * 10 + a2 % 10;
+        a2 /= 10;
+    }
+
+    if (r1 > r2) {
+        printf("%d", r1);
+    }
+    else {
+        printf("%d", r2);
+    }
+
+    return 0;
+}
