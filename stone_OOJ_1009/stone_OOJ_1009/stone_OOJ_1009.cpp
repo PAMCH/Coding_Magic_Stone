68b6cf1ad64b71dfb99bdd1038091681a8f409c0
PAMCH <pamic7124@naver.com>
2023-02-07 21:44:03 +0900

    Solved

---
+++ stone_OOJ_1009/stone_OOJ_1009/stone_OOJ_1009.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, sum = 0;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    for (int i = a; i <= b; i++) {
+        if (i % 2 == 1) {
+            sum += i;
+        }
+    }
+
+    //printf("%d\n", ((b + 1) / 2) * ((b + 1) / 2) - (a / 2) * (a / 2));
+
+    printf("%d\n", sum);
+
+    return 0;
+}
