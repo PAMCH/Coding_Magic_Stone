eb8cdd4e389593066f103ddccc375fa735679e1e
PAMCH <pamic7124@naver.com>
2023-02-07 22:59:43 +0900

    Solved

---
+++ stone_OOJ_2016/stone_OOJ_2016/stone_OOJ_2016.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+    int a, b, sum = 0;
+
+    scanf("%d %d", &a, &b);
+
+    sum = a;
+    while (a >= b) {
+        sum += a / b;
+        a = a / b + a & b;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
