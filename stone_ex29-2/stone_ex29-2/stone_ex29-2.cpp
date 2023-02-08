c2605f537d697321148a5ce6a0f2129ed202c3fa
PAMCH <pamic7124@naver.com>
2023-02-08 22:56:05 +0900

    Solved

---
+++ stone_ex29-2/stone_ex29-2/stone_ex29-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[] = { 5, 7, 13, 11, 6, 10, 45, 11, 4, 9 }, sum = 0;
+
+	for (int i = 0; i < 10; i++) {
+		if (a[i] % 2 == 0) {
+			sum += a[i];
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
