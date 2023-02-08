551155b45e184f0f7d7aed2cf7a834ba1c48b832
PAMCH <pamic7124@naver.com>
2023-02-08 23:20:42 +0900

    Solved

---
+++ stone_OOJ_1094/stone_OOJ_1094/stone_OOJ_1094.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, n[5];
+
+	scanf("%d %d", &a, &b);
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &n[i]);
+	}
+
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", n[i] - (a * b));
+	}
+
+	return 0;
+}
