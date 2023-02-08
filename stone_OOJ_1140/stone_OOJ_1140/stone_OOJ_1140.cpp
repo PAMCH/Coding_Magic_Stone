08119d12180258756c2af504182fbf6848426b73
PAMCH <pamic7124@naver.com>
2023-02-08 21:39:45 +0900

    Solved

---
+++ stone_OOJ_1140/stone_OOJ_1140/stone_OOJ_1140.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, c, cnt, sum = 0, pcnt = 0, n = -1;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	for (int i = a; i <= b; i++) {
+		cnt = 0;
+		for (int j = 1; j <= i; j++) {
+			if (i % j == 0) {
+				cnt++;
+			}
+		}
+
+		if (cnt == 2) {
+			sum += i;
+			pcnt++;
+
+			if (pcnt == c) {
+				n = i;
+			}
+		}
+	}
+	printf("%d\n%d\n", sum, n);
+
+	return 0;
+}
