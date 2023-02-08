32337274ab1951cf9d7fb4b11f93763614515cd9
PAMCH <pamic7124@naver.com>
2023-02-08 22:20:27 +0900

    Modified

---
+++ stone_ex27-2/stone_ex27-2/stone_ex27-2.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, sum = 0;
+
+	while (1) {
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0) {
+			break;
+		}
+		if (a + b >= sum) {
+			sum = a + b;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
