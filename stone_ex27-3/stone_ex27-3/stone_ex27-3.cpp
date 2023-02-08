d023522c3fb203ff1c3bd1311bf40d0a1b2c8c05
PAMCH <pamic7124@naver.com>
2023-02-08 22:24:40 +0900

    Solved

---
+++ stone_ex27-3/stone_ex27-3/stone_ex27-3.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int cnt=0;
+
+	for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 0 || i % 3 == 0) {
+			continue;
+		}
+		printf("%d ", i);
+		cnt++;
+		if (cnt % 10 == 0) {
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
