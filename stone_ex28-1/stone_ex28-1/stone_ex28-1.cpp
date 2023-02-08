5ef095cc834624521989feb9c83b57a0b9f5f925
PAMCH <pamic7124@naver.com>
2023-02-08 22:43:19 +0900

    Solved

---
+++ stone_ex28-1/stone_ex28-1/stone_ex28-1.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, cnt = 0;
+
+	scanf("%d", &a);
+
+	while (a != 1) {
+		if (a % 2 == 0) {
+			a /= 2;
+			cnt++;
+		}
+		else {
+			a = (a + 1) / 2;
+			cnt++;
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
