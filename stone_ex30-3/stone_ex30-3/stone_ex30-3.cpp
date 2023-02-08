23173c5dfe8ab9d3482305d24fc133b04156ba73
PAMCH <pamic7124@naver.com>
2023-02-08 23:39:53 +0900

    Solved

---
+++ stone_ex30-3/stone_ex30-3/stone_ex30-3.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[51], n = 0;
+	int i, j, k, cnt;
+
+	for (i = 2; i <= 50; i++) {
+		cnt = 0;
+		k = sqrt(i);
+		for (j = 2; j <= k; j++) {
+			if (i % j == 0) {
+				cnt++;
+			}
+		}
+
+		if (cnt == 0) {
+			a[++n] = i;
+		}
+	}
+
+	for (i = 1; i <= n; i++) {
+		printf("%d\n", a[i]);
+	}
+	return 0;
+}
