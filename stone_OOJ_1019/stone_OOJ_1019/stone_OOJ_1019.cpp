3c9d52938a4d91227276546ff96b1da016ccd2e3
PAMCH <pamic7124@naver.com>
2023-02-08 23:01:38 +0900

    Solved

---
+++ stone_OOJ_1019/stone_OOJ_1019/stone_OOJ_1019.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, even = 0, odd = 0;
+	scanf("%d", &a);
+
+	int n[100];
+
+	for (int i = 0; i < a; i++) {
+		scanf("%d", &n[i]);
+		if (n[i] % 2 == 0) {
+			even++;
+		}
+		else {
+			odd++;
+		}
+	}
+
+	printf("%d\n%d\n", even, odd);
+
+
+	return 0;
+}
