c22dcebab9459375baa965225d9a11d112e988f4
PAMCH <pamic7124@naver.com>
2023-02-08 22:02:35 +0900

    Solved

---
+++ stone_OOJ_1142/stone_OOJ_1142/stone_OOJ_1142.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, cnt;
+
+	scanf("%d", &a);
+
+	for (int i = 4; i - 1 <= a; i *= 2) {
+		cnt = 0;
+		for (int j = 1; j <= i - 1; j++) {
+			if ((i - 1) % j == 0) {
+				cnt++;
+			}
+		}
+
+		if (cnt == 2) {
+			printf("%d\n", i - 1);
+		}
+	}
+
+	return 0;
+}
