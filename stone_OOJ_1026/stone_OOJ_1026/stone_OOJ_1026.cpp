7866964069c6dc36476525e346b6082c74b0ca05
PAMCH <pamic7124@naver.com>
2023-02-08 23:17:43 +0900

    Solved

---
+++ stone_OOJ_1026/stone_OOJ_1026/stone_OOJ_1026.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[6], b[] = { 1,1,2,2,2,8 };
+
+	for (int i = 0; i < 6; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < 6; i++) {
+		printf("%d ", b[i] - a[i]);
+	}
+	
+
+	return 0;
+}
