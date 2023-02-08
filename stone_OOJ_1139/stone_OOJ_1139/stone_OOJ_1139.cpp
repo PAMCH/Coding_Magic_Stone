c75bb15d9b23c2f5767a484084dd362af34781a1
PAMCH <pamic7124@naver.com>
2023-02-08 23:26:48 +0900

    Solved

---
+++ stone_OOJ_1139/stone_OOJ_1139/stone_OOJ_1139.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, c;
+	int x, cnt[10] = { 0 };
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	x = a * b * c;
+	while (x) {
+		cnt[x % 10]++;
+		x /= 10;
+	}
+
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", cnt[i]);
+	}
+	return 0;
+}
