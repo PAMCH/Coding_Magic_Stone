f8c4ab3bb4d4aa6e1473ca86d980ed9430164d07
PAMCH <pamic7124@naver.com>
2023-02-08 22:38:05 +0900

    Solved

---
+++ stone_OOJ_1046/stone_OOJ_1046/stone_OOJ_1046.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, day = 0, ans = 0;
+
+	while (true) {
+		scanf("%d %d", &a, &b);
+		if (a == 0 && b == 0) {
+			break;
+		}
+
+		day++;
+
+		if (a + b > 8 && ans == 0) {
+			ans = day;
+			// break; 입력 중간에 끊겨가지고 조건을 바꿈
+		}
+	}
+
+	printf("%d\n", ans);
+
+	return 0;
+}
