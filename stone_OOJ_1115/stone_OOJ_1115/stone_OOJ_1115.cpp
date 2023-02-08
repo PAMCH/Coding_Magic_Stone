1c7e62f3e1c91706d5bb82f864c6c1e723b04b5b
PAMCH <pamic7124@naver.com>
2023-02-08 23:46:28 +0900

    Solved

---
+++ stone_OOJ_1115/stone_OOJ_1115/stone_OOJ_1115.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, n[51];
+	int i, cnt = 0;
+
+	scanf("%d %d", &a, &b);
+	for (i = 1; i <= a; i++) {
+		scanf("%d", &n[i]);
+	}
+	for (i = 1; i <= a; i++) {
+		if (n[i] > 0 && n[b] <= n[i]) {
+			cnt++;
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
