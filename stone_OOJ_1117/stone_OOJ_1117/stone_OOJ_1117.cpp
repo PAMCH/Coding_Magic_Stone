f34fc191efa13aa9f02076d79e398cb4f80c2eaf
PAMCH <pamic7124@naver.com>
2023-02-08 23:49:54 +0900

    Solved

---
+++ stone_OOJ_1117/stone_OOJ_1117/stone_OOJ_1117.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, n[101];
+	int i, sum = 0;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+	for (i = 1; i <= b; i++) {
+		scanf("%d", &n[i]);
+	}
+
+	for (i = 1; i <= b; i++) {
+		sum += a;
+		sum -= n[i];
+	}
+
+	printf("%d\n", sum + a);
+
+	return 0;
+}
