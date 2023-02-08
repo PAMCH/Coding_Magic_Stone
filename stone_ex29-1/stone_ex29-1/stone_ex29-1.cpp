925e74b08c4e5829121b7ed6527254cb52850bdc
PAMCH <pamic7124@naver.com>
2023-02-08 22:55:02 +0900

    Solved

---
+++ stone_ex29-1/stone_ex29-1/stone_ex29-1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[] = { 4, 7, 6, 8, 11, -3, 8, 11, 5, 13 }, sum = 0;
+
+	for (int i = 0; i < 10; i++) {
+		sum += a[i];
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
