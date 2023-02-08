8e7d0927cb9691433638f3b3d2a5b8c71148f3fd
PAMCH <pamic7124@naver.com>
2023-02-08 22:13:44 +0900

    Modified

---
+++ stone_ex27-1/stone_ex27-1/stone_ex27-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int sum = 0;
+
+	for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 0) {
+			printf("%d ", i);
+			sum += i;
+		}
+		if (sum > 50) {
+			printf("\n");
+			break;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
