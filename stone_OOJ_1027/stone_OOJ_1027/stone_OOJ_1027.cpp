5963e1a6b3fbd5228509c807e19503e90b90d0af
PAMCH <pamic7124@naver.com>
2023-02-08 22:46:40 +0900

    Solved

---
+++ stone_OOJ_1027/stone_OOJ_1027/stone_OOJ_1027.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, max;
+
+	scanf("%d", &a);
+
+	max = a;
+
+	while (a != 1) {
+		if (a % 2 == 0) {
+			a /= 2;
+		}
+		else {
+			a = (a * 3) + 1;
+		}
+		
+		if (max < a) {
+			max = a;
+		}
+	}
+
+	printf("%d\n", max);
+
+	return 0;
+}
