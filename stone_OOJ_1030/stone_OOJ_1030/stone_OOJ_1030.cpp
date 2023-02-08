ff8766b15297b565c9d07bc7079b55e5c50f5217
PAMCH <pamic7124@naver.com>
2023-02-08 23:14:38 +0900

    Solved

---
+++ stone_OOJ_1030/stone_OOJ_1030/stone_OOJ_1030.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, val;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &val);
+		printf("%2d ", val);
+		for (int j = 1; j <= val; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
