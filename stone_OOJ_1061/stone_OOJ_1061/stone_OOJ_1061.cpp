804651b9818b564b191c33a532b559263f9f6905
PAMCH <pamic7124@naver.com>
2023-02-09 23:18:43 +0900

    Solved

---
+++ stone_OOJ_1061/stone_OOJ_1061/stone_OOJ_1061.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a[11];
+    int i, sum = 0, ans;
+
+    for (i = 1; i <= 10; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i <= 10; i++) {
+        sum += a[i];
+        if (sum <= 100) {
+            ans = sum;
+        }
+        else {
+            if (100 - ans >= sum - 100) {
+                ans = sum;
+            }
+            break;
+        }
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
