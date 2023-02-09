cd0f61a629deee2fb3d2fade96e91881f9fc0756
PAMCH <pamic7124@naver.com>
2023-02-09 22:10:04 +0900

    Solved

---
+++ stone_OOJ_1023/stone_OOJ_1023/stone_OOJ_1023.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, a[101];
+    int i, max, min;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    max = min = a[1];
+
+    for (i = 2; i <= n; i++) {
+        if (max < a[i]) {
+            max = a[i];
+        }
+        else if (min > a[i]) {
+            min = a[i];
+        }
+    }
+
+    printf("%d\n%d\n", max, min);
+
+    return 0;
+}
