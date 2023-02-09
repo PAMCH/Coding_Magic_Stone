1da2e5d61e27b63149946bfe6d9e213c422cc363
PAMCH <pamic7124@naver.com>
2023-02-09 22:05:28 +0900

    Solved

---
+++ stone_ex31-1/stone_ex31-1/stone_ex31-1.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a[11] = { 0, 7, -5, 4, -99, 45, 11, 0, 8, 50, 77 };
+    int i, min, max;
+
+    min = max = a[1];
+
+    for (i = 2; i <= 10; i++) {
+        if (max < a[i]) {
+            max = a[i];
+        }
+        else if (min > a[i]) {
+            min = a[i];
+        }
+    }
+
+    printf("%d\n%d\n", max, min);
+
+
+    return 0;
+}
