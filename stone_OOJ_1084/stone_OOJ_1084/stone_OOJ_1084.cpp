40b29d37b5adce23a59840694291b28cbf035e58
PAMCH <pamic7124@naver.com>
2023-02-09 21:41:01 +0900

    Solved

---
+++ stone_OOJ_1084/stone_OOJ_1084/stone_OOJ_1084.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n = 0, a[16];
+    int val, i, j, cnt = 0;
+
+    while (true) {
+        scanf("%d", &val);
+
+        if (val == 0) {
+            break;
+        }
+
+        a[++n] = val;
+    }
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            if (a[i] * 2 == a[j]) {
+                cnt++;
+            }
+        }
+    }
+
+    printf("%d\n", cnt);
+
+    return 0;
+}
