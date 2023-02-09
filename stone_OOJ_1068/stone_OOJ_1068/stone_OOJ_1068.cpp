bb5d60962fa912985aeb0ad6600f10e537bbe851
PAMCH <pamic7124@naver.com>
2023-02-09 22:23:43 +0900

    Solved

---
+++ stone_OOJ_1068/stone_OOJ_1068/stone_OOJ_1068.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int s1, s2, s3, s4;
+    int i, k, max = 0;
+
+    for (i = 1; i <= 5; i++) {
+        scanf("%d %d %d %d", &s1, &s2, &s3, &s4);
+
+        if (max < s1 + s2 + s3 + s4) {
+            max = s1 + s2 + s3 + s4;
+            k = i;
+        }
+    }
+
+    printf("%d %d\n", k, max);
+
+    return 0;
+}
