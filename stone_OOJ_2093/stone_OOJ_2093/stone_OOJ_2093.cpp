77fdc51bd1e8025b6f3e1c2b2d753977ed4c25d3
PAMCH <pamic7124@naver.com>
2023-02-09 23:32:05 +0900

    Solved

---
+++ stone_OOJ_2093/stone_OOJ_2093/stone_OOJ_2093.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, a[21];
+    int i, x, y;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    x = y = a[1];
+
+    for (i = 1; i <= n; i++) {
+        if (x > a[i]) {
+            x = a[i];
+        }
+        else if (y < a[i]) {
+            y = a[i];
+        }
+    }
+
+    printf("%d\n", (y - x) * 2);
+
+    return 0;
+}
