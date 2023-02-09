a2d21659ca122aa3134ac89e9b5540ed15f23480
PAMCH <pamic7124@naver.com>
2023-02-09 21:36:17 +0900

    Solved

---
+++ stone_OOJ_1121/stone_OOJ_1121/stone_OOJ_1121.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, x, p1, p2, p3;
+    int i, sum = 0;
+
+    scanf("%d", &n);
+    scanf("%d", &x);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d %d", &p1, &p2);
+
+        if (p1 <= p2 && x >= p2 - p1) {
+            sum += p2;
+        }
+        else if (p2 < p1 && x >= p1 - p2) {
+            sum += p1;
+        }
+        else {
+            scanf("%d\n", &p3);
+            sum += p3;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
