6fa572c421d8eb51d3b5de6180fd20257548a15a
PAMCH <pamic7124@naver.com>
2023-02-09 21:22:24 +0900

    Solved

---
+++ stone_OOJ_2010/stone_OOJ_2010/stone_OOJ_2010.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, a[51];
+    int i, sum = 0, ave, res = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    ave = sum / n;
+
+    for (i = 1; i <= n; i++) {
+        if (a[i] > ave) {
+            res += (a[i] - ave);
+        }
+    }
+
+    printf("%d\n", res);
+    
+    return 0;
+}
