2f51b12c10ad7a0714c76ad882b876ad0600418d
PAMCH <pamic7124@naver.com>
2023-02-09 21:53:22 +0900

    Solved

---
+++ stone_OOJ_1104/stone_OOJ_1104/stone_OOJ_1104.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int p, q;
+    int n = 0, a[101], m = 0, b[101];
+    int i, j;
+
+    scanf("%d %d", &p, &q);
+
+    for (i = 1; i <= p; i++) {
+        if (p % i == 0) {
+            a[++n] = i;
+        }
+    }
+    for (i = 1; i <= q; i++) {
+        if (q % i == 0) {
+            b[++m] = i;
+        }
+    }
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            printf("%d %d\n", a[i], b[j]);
+        }
+    }
+    return 0;
+}
