26c56fce975805ad8fcce466ed735be1fe22a367
PAMCH <pamic7124@naver.com>
2023-02-09 21:58:00 +0900

    Solved

---
+++ stone_OOJ_2022/stone_OOJ_2022/stone_OOJ_2022.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int x, y;
+    int n = 0, a[11], m = 0, b[11];
+    int i, j, sum = 0;
+
+    scanf("%d %d", &x, &y);
+
+    while (x) {
+        a[++n] = x % 10;
+        x /= 10;
+    }
+    while (y) {
+        b[++m] = y % 10;
+        y /= 10;
+    }
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= m; j++) {
+            sum += a[i] * b[j];
+        }
+    }
+    
+    printf("%d\n", sum);
+
+    return 0;
+}
