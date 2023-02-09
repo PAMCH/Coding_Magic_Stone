71d8ddb03c6160055d3d1493cc688452cdae995a
PAMCH <pamic7124@naver.com>
2023-02-09 21:28:00 +0900

    Solved

---
+++ stone_OOJ_2137/stone_OOJ_2137/stone_OOJ_2137.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, a[101];
+    int i, b, sum = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i <= n; i++) {
+        b = a[i] * i - sum;
+        printf("%d ", b);
+        sum += b;
+    }
+
+    return 0;
+}
