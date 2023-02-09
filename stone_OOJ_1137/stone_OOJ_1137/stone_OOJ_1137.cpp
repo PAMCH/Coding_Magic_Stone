e8051f40da339d6c385f2a763983f38a599bbd1c
PAMCH <pamic7124@naver.com>
2023-02-09 22:12:49 +0900

    Solved

---
+++ stone_OOJ_1137/stone_OOJ_1137/stone_OOJ_1137.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int a[10];
+    int i, k = 1;
+
+    for (i = 1; i <= 9; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 2; i <= 9; i++) {
+        if (a[k] < a[i]) {
+            k = i;
+        }
+    }
+
+    printf("%d\n%d\n", a[k], k);
+
+    return 0;
+}
