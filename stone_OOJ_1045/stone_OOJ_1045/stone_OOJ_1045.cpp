fe44c1e5e1b0167afd9a0ce5fde374bb2b4b629c
PAMCH <pamic7124@naver.com>
2023-02-09 23:10:35 +0900

    Solved

---
+++ stone_OOJ_1045/stone_OOJ_1045/stone_OOJ_1045.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int n, x[10001] = { 0 };
+    int i, val, max, k = 0;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        scanf("%d", &val); 
+        x[val]++;
+    }
+
+    max = x[1], k = 1;
+    
+    for (i = 1; i <= 10000; i++) {
+        if (max < x[i]) {
+            max = x[i];
+            k = i;
+        }
+    }
+
+    printf("%d\n", k);
+    
+    return 0;
+}
