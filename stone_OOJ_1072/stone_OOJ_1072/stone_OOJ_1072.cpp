dcb73fa296c6d8210ce6a1e76f0597ca8883777a
PAMCH <pamic7124@naver.com>
2023-02-10 22:49:54 +0900

    Solved

---
+++ stone_OOJ_1072/stone_OOJ_1072/stone_OOJ_1072.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, s[11], t[11] = { 0 };
+	int i, sum = 0;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d %d", &s[i], &t[i]);
+		sum += s[i] * (t[i] - t[i - 1]);
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
