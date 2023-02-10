24b2b536f9ec5bfb1cafa64f92cdfd7dc10e8643
PAMCH <pamic7124@naver.com>
2023-02-10 22:02:06 +0900

    Solved

---
+++ stone_OOJ_2017/stone_OOJ_2017/stone_OOJ_2017.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[4], i, j, temp;
+
+	for (i = 1; i <= 3; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (i = 1; i <= 2; i++) {
+		for (j = i + 1; j <= 3; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i], arr[i] = arr[j], arr[j] = temp;
+			}
+		}
+	}
+
+	if (arr[2] - arr[1] <= arr[3] - arr[2]) {
+		printf("%d\n", arr[3] - arr[2] - 1);
+	}
+	else {
+		printf("%d\n", arr[2] - arr[1] - 1);
+	}
+
+	return 0;
+}
