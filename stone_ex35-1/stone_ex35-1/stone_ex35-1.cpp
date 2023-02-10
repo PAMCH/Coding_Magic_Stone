35d3d538d30c2e086513682300f4a5a9b7a3a064
PAMCH <pamic7124@naver.com>
2023-02-10 22:42:16 +0900

    Solved

---
+++ stone_ex35-1/stone_ex35-1/stone_ex35-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, i, arr[21] = { 0,1 };
+
+	scanf("%d", &n);
+
+	for (i = 2; i <= n; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	printf("%d\n", arr[n]);
+
+	return 0;
+}
