de17386831476ec79e1474d894285c9b097031a3
PAMCH <pamic7124@naver.com>
2023-02-10 23:14:58 +0900

    Solved

---
+++ stone_OOJ_1066/stone_OOJ_1066/stone_OOJ_1066.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n;
+	int i, c[31] = { 0 };
+
+	for (i = 1; i <= 28; i++) {
+		scanf("%d", &n);
+		c[n] = 1;
+	}
+
+	for (i = 1; i <= 30; i++) {
+		if (c[i] == 0) {
+			printf("%d\n", i);
+		}
+	}
+
+	return 0;
+}
