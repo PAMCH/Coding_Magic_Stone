4769420d8bcab284ceb33a8468292e998937d911
PAMCH <pamic7124@naver.com>
2023-02-10 22:37:34 +0900

    Solved

---
+++ stone_ex34-1/stone_ex34-1/stone_ex34-1.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[11] = { 0,1,2,3,4,5,6,7,8,9,10 }, i, j, key;
+
+	for (i = 2; i <= 10; i++) {
+		key = arr[i];
+		for (j = i - 1; j >= 1 && arr[j] < key; j--) {
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = key;
+	}
+
+	for (i = 1; i <= 10; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
