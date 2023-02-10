ed4ba70d85f334ef6ac9f30e0b84d3906b84ddfc
PAMCH <pamic7124@naver.com>
2023-02-10 23:20:07 +0900

    Solved

---
+++ stone_OOJ_1038/stone_OOJ_1038/stone_OOJ_1038.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int num;
+	int i, check[42] = { 0 }, cnt = 0;
+
+	for (i = 1; i <= 10; i++) {
+		scanf("%d", &num);
+		check[num % 42] = 1;
+	}
+	for (i = 0; i <= 41; i++) {
+		cnt += check[i];
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
