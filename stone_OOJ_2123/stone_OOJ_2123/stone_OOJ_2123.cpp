df8fac805dfdebf73e59e8bb6c53d6f6f46fd954
PAMCH <pamic7124@naver.com>
2023-02-10 22:08:09 +0900

    Solved

---
+++ stone_OOJ_2123/stone_OOJ_2123/stone_OOJ_2123.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[4], i, j, temp;
+
+	for (i = 1; i <= 3; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (i = 1; i <= 2; i++) {
+		for (j = i + 1; j <= 3; j++) {
+			if (arr[i] > arr[j]) {
+				temp = arr[i], arr[i] = arr[j], arr[j] = temp;
+			}
+		}
+	}
+
+	if (arr[3] - arr[2] == arr[2] - arr[1]) {
+		printf("%d\n", arr[3] + arr[2] - arr[1]);
+	}
+	else if (arr[3] - arr[2] == (arr[2] - arr[1]) * 2) {
+		printf("%d\n", (arr[3] + arr[2]) / 2);
+	}
+	else {
+		printf("%d\n", (arr[2] + arr[1]) / 2);
+	}
+
+	return 0;
+}
