ce01a7a2a700e48cb1d93607b55d24026497cbd9
PAMCH <pamic7124@naver.com>
2023-02-10 21:51:27 +0900

    Solved

---
+++ stone_OOJ_1025/stone_OOJ_1025/stone_OOJ_1025.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, arr[11];
+	int i, j, temp;
+
+	for (i = 1; i <= 10; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (i = 1; i <= 9; i++) {
+		for (j = i + 1; j <= 10; j++) {
+			if (arr[i] < arr[j]) {
+				temp = arr[i], arr[i] = arr[j], arr[j] = temp;
+			}
+		}
+	}
+
+	printf("%d\n", arr[3]);
+
+	return 0;
+}
