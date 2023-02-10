568d52f8610a6d85d1002e01717c58bdae5081ba
PAMCH <pamic7124@naver.com>
2023-02-10 23:47:27 +0900

    Solved

---
+++ stone_OOJ_2079/stone_OOJ_2079/stone_OOJ_2079.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int len, m, s, e;
+	int i, n;
+
+	scanf("%d %d", &len, &m);
+
+	n = len + 1;
+
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d", &s, &e);
+		n -= (e - s + 1);
+	}
+
+	printf("%d\n", n);
+
+	return 0;
+}
