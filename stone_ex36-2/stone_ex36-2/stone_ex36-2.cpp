decef2e8cd4d986a1850b69f256e20bb72976891
PAMCH <pamic7124@naver.com>
2023-02-10 23:12:23 +0900

    Solved

---
+++ stone_ex36-2/stone_ex36-2/stone_ex36-2.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int c[101] = { 0 }, i, j, cnt = 0;
+
+	for (i = 2; i <= 100; i++) {
+		if (c[i] == 0) {
+			for (j = i * i; j <= 100; j += i) {
+				c[j] = i;
+			}
+		}
+	}
+
+	for (i = 2; i <= 98; i++) {
+		if (c[i] == 0 && c[i + 2] == 0) {
+			printf("%d %d\n", i, i + 2);
+		}
+	}
+
+	return 0;
+}
