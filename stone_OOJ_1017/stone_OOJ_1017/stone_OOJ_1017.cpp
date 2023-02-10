3857833eb0236beb6fb9a9b33682126e4089b78e
PAMCH <pamic7124@naver.com>
2023-02-10 22:46:34 +0900

    Solved

---
+++ stone_OOJ_1017/stone_OOJ_1017/stone_OOJ_1017.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, i, arr[100000], max;
+
+	scanf("%d", &n);
+
+	arr[1] = arr[2] = max = 1;
+
+	for (i = 3; i <= n; i++) {
+		if (i % 2) {
+			arr[i] = arr[i / 2] + arr[i / 2 + 1];
+		}
+		else {
+			arr[i] = arr[i / 2];
+		}
+
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+	}
+
+	printf("%d\n", max);
+
+	return 0;
+}
