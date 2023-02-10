ed06bc5ecc82ab3bb9937cb92cbf013f545b6605
PAMCH <pamic7124@naver.com>
2023-02-10 23:33:32 +0900

    Solved

---
+++ stone_OOJ_2031/stone_OOJ_2031/stone_OOJ_2031.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, m, op, s, e;
+	int i, j, check[501] = { 0 }, cnt;
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i <= m; i++) {
+		scanf("%d %d %d", &op, &s, &e);
+		
+		if (op == 0) {
+			for (j = s; j <= e; j++) {
+				check[j] = 1 - check[j];
+			}
+		}
+		else {
+			cnt = 0;
+			for (j = s; j <= e; j++) {
+				cnt += check[j];
+			}
+			printf("%d\n", cnt);
+		}
+	}
+
+	return 0;
+}
