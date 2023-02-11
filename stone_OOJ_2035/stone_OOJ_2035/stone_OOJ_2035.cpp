dbab48f0590ecfe26edd0a0efb2642e62e3944a2
PAMCH <pamic7124@naver.com>
2023-02-11 13:44:25 +0900

    Solved

---
+++ stone_OOJ_2035/stone_OOJ_2035/stone_OOJ_2035.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int m, t, u, f, d;
+	char arr[100001];
+	int i, sum = 0;
+
+	scanf("%d %d %d %d %d", &m, &t, &u, &f, &d);
+	for (i = 1; i <= t; i++) {
+		scanf(" %c", &arr[i]);
+	}
+
+	for (i = 1; i <= t; i++) {
+		if (arr[i] == 'u' || arr[i] == 'd') {
+			sum += (u + d);
+		}
+		else {
+			sum += f * 2;
+		}
+
+		if (sum > m) {
+			break;
+		}
+	}
+
+	printf("%d\n", i - 1);
+
+	return 0;
+}
