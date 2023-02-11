c3191a317b6ca2796bd84a0b3a3efaa4b0e098f5
PAMCH <pamic7124@naver.com>
2023-02-11 14:08:18 +0900

    Solved

---
+++ stone_OOJ_1076/stone_OOJ_1076/stone_OOJ_1076.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[9];
+	int i, as, ds;
+
+	for (i = 1; i <= 8; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (as = 1; as <= 7 && arr[as] < arr[as + 1]; as++);
+	for (ds = 1; ds <= 7 && arr[ds] > arr[ds + 1]; ds++);
+
+	if (as == 8) {
+		printf("ascending\n");
+	}
+	else if (ds == 8) {
+		printf("descending\n");
+	}
+	else {
+		printf("mixed\n");
+	}
+
+	return 0;
+}
