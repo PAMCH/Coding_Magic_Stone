6ed499ec1bc0c5b1059fc2ca06a84a45d9156074
PAMCH <pamic7124@naver.com>
2023-02-11 12:52:37 +0900

    Solved

---
+++ stone_ex38-1/stone_ex38-1/stone_ex38-1.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[11] = { 0,10,20,30,40,50,60,70,80,90,100 };
+	int t, s, e, i, sum[11] = { 0 };
+
+	for (i = 1; i <= 10; i++) {
+		sum[i] = sum[i - 1] + arr[i];
+	}
+
+	scanf("%d", &t);
+
+	for (i = 1; i <= t; i++) {
+		scanf("%d %d", &s, &e);
+		printf("%d\n", sum[e] - sum[s - 1]);
+	}
+
+	return 0;
+}
