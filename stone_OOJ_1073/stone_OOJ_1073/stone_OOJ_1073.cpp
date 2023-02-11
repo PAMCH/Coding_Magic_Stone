1a1a4a3997dc19fea5cf79b49cf2a8d1bcb8b3ab
PAMCH <pamic7124@naver.com>
2023-02-11 12:29:07 +0900

    Solved

---
+++ stone_OOJ_1073/stone_OOJ_1073/stone_OOJ_1073.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	int i, check[10001] = { 0 };
+
+	for (i = 1; i * i + i * (i - 1) / 2 <= 10000; i++) {
+		check[i * i + i * (i - 1) / 2] = 1;
+	}
+
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++) {
+		scanf("%d", &b);
+		if (check[b] == 1) {
+			printf("%d Y\n", b);
+		}
+		else {
+			printf("%d N\n", b);
+		}
+	}
+
+	return 0;
+}
