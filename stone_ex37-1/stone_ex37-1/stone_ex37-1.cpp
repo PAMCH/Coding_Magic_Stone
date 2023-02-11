add5b80791168507631151fec119b5758c0f54ee
PAMCH <pamic7124@naver.com>
2023-02-11 12:15:48 +0900

    Solved

---
+++ stone_ex37-1/stone_ex37-1/stone_ex37-1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int main()
+{
+	int n, s1, s2, s3;
+	scanf("%d", &n);
+
+	s1 = (n + 1) * n / 2;
+	s2 = n * n;
+	s3 = n * n + n * (n - 1) / 2;
+
+	printf("%d\n", s1 + s2 + s3);
+
+	return 0;
+}
