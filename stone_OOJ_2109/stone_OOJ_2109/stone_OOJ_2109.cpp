457439c38b156c87dd4be3e01a3220947747ffa9
PAMCH <pamic7124@naver.com>
2023-02-11 13:12:16 +0900

    Solved

---
+++ stone_OOJ_2109/stone_OOJ_2109/stone_OOJ_2109.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, k, arr[100001];
+	int i, sum[100001] = { 0 }, s, e, max;
+
+	scanf("%d %d", &n, &k);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+		sum[i] = sum[i - 1] + arr[i];
+	}
+
+	max = sum[k];
+
+	for (i = 1; i <= n - k + 1; i++) {
+		if (max < sum[i + k - 1] - sum[i - 1]) {
+			max = sum[i + k - 1] - sum[i - 1];
+		}
+	}
+	printf("%d\n", max);
+
+	return 0;
+}
