20b00bbb893ad24478ee158f6168ecd902b213b4
PAMCH <pamic7124@naver.com>
2023-02-11 14:17:50 +0900

    Solved

---
+++ stone_OOJ_2069/stone_OOJ_2069/stone_OOJ_2069.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, x, y, i, arr[101], cnt = 0, max = 0;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d %d", &x, &y);
+		arr[i] = x + y;
+	}
+
+	for (i = 1; i <= n; i++) {
+		if (arr[i] == 0) {
+			cnt++;
+		}
+		else {
+			cnt = 0;
+		}
+
+		if (max < cnt) {
+			max = cnt;
+		}
+	}
+
+	printf("%d\n", max * 2);
+
+	return 0;
+}
