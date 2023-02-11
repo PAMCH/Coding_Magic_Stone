d39db7adf355f3a924f92d4a6c0fbe31f50d887c
PAMCH <pamic7124@naver.com>
2023-02-11 14:02:12 +0900

    Solved

---
+++ stone_OOJ_1078/stone_OOJ_1078/stone_OOJ_1078.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, arr[81];
+	int i, prev, cnt = 0;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	prev = arr[1];
+
+	for (i = 2; i <= n; i++) {
+		if (prev != arr[i]) {
+			cnt++, prev = arr[i];
+		}
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
