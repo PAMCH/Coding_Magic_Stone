f86346efe8f076de6b244981b498cd4e19138085
PAMCH <pamic7124@naver.com>
2023-02-11 13:34:25 +0900

    Solved

---
+++ stone_OOJ_1049/stone_OOJ_1049/stone_OOJ_1049.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a, b, c, res;
+	char op1, op2;
+
+	scanf("%d %c %d %c %d", &a, &op1, &b, &op2, &c);
+
+	if (op1 == '+') {
+		res = a + b;
+	}
+	else if (op1 == '-') {
+		res = a - b;
+	}
+	else if (op1 == '*') {
+		res = a * b;
+	}
+	else {
+		res = a / b;
+	}
+
+	if (op2 == '+') {
+		res += c;
+	}
+	else if (op2 == '-') {
+		res -= c;
+	}
+	else if (op2 == '*') {
+		res *= c;
+	}
+	else {
+		res /= c;
+	}
+
+	printf("%d\n", res);
+
+	return 0;
+}
