7d6bf2f7f9e379ccffa7c1692af9f0332d704fe2
PAMCH <pamic7124@naver.com>
2023-02-11 13:57:41 +0900

    Solved

---
+++ stone_ex40-1/stone_ex40-1/stone_ex40-1.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[11];
+	int i, prev = -1, cnt, max = 0, k;
+
+	for (i = 1; i <= 10; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (i = 1; i <= 10; i++) {
+		if (prev == arr[i]) {
+			cnt++;
+		}
+		else {
+			prev = arr[i], cnt = 1;
+		}
+
+		if (max < cnt) {
+			max = cnt, k = prev;
+		}
+
+
+	}
+
+	printf("%d\n%d\n", k, max);
+
+	return 0;
+}
