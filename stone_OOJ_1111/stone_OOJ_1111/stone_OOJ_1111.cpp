62f689170181c19f58c2d4ea0304a976a3041bbf
PAMCH <pamic7124@naver.com>
2023-02-11 12:43:17 +0900

    Solved

---
+++ stone_OOJ_1111/stone_OOJ_1111/stone_OOJ_1111.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, i, j, min, r, c;
+	scanf("%d", &n);
+
+	min = n + n;
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (i * j >= n) {
+				if (min > i + j || (min == i + j && r > i)) {
+					min = i + j, r = i, c = j;
+				}
+			}
+		}
+	}
+
+	printf("%d %d\n", r, c);
+
+	return 0;
+}
