6ab1b22a515b9d06dc52b51b5efb8e7a86eb18ed
PAMCH <pamic7124@naver.com>
2023-02-11 12:35:04 +0900

    Solved

---
+++ stone_OOJ_1077/stone_OOJ_1077/stone_OOJ_1077.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, i, j;
+
+	scanf("%d", &n);
+
+	printf(" *");
+
+	for (i = 1; i <= n; i++) {
+		printf("%4d", i);
+	}
+	printf("\n");
+
+	for (i = 1; i <= n; i++) {
+		printf("%2d", i);
+		for (j = 1; j <= n; j++) {
+			printf("%4d", i * j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
