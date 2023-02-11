d6d1aa51836a1ac61d4ff7c46c764a3086a38df7
PAMCH <pamic7124@naver.com>
2023-02-11 13:07:01 +0900

    Solved

---
+++ stone_OOJ_2025/stone_OOJ_2025/stone_OOJ_2025.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, q, arr[50001], s, e;
+	int i, sum[50001] = { 0 };
+
+	scanf("%d %d", &n, &q);
+
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+		sum[i] = sum[i - 1] + arr[i];
+	}
+
+	for (i = 1; i <= q; i++) {
+		scanf("%d %d", &s, &e);
+		printf("%d\n", sum[e] - sum[s - 1]);
+	}
+
+	return 0;
+}
