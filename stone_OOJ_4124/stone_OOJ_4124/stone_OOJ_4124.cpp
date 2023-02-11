e68d2699b468188bd206d2f0815914d95182a405
PAMCH <pamic7124@naver.com>
2023-02-11 14:37:21 +0900

    Solved

---
+++ stone_OOJ_4124/stone_OOJ_4124/stone_OOJ_4124.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, sum = 0, k, i, j, cnt, pcnt = 0, arr[30001] = { 0 };
+
+	scanf("%d", &n);
+
+	for (i = 2; i <= n; i++) {
+		k = sqrt(i);
+		cnt = 0;
+		
+		for (j = 2; j <= k; j++) {
+			if (i % j == 0) {
+				cnt++;
+			}
+		}
+
+		if (cnt == 0) {
+			arr[i] = 1;
+		}
+	}
+
+	for (i = 2; i <= n-1; i++) {
+		for (j = i; j <= n; j++) {
+			if (arr[i] == 1 && arr[j] == 1 && i + j == n) {
+				printf("%d %d\n", i, j);
+				pcnt++;
+			}
+		}
+	}
+	
+	printf("%d\n", pcnt);
+	return 0;
+}
